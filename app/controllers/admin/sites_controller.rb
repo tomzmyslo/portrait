3ad05a0a89ba2fa255b4cbbe7c8e3a5aef2ab633
Tom Zmyslo <tom@zmyslo.com>
2018-07-05 22:03:07 -0400

    Deleted last HAML template

--- app/controllers/admin/sites_controller.rb
+++ app/controllers/admin/sites_controller.rb
@@ -1,5 +1,5 @@
 class Admin::SitesController < ApplicationController
-  # before_action :is_admin
+  before_action :is_admin
 
   def index
     @sites = Site.order(created_at: :desc).paginate(page: params[:page], per_page: 20)
